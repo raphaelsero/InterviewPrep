2f9ca5e9ec2131a94ad59845eeb619ae4125aebd
Raphael Cabral <raphaelserocabral@gmail.com>
2022-11-05 11:51:35 -0700

    Recursive Backtrack
    If the input is empty, return an empty array.
    
    Initialize a data structure (e.g. a hash map) that maps digits to their letters, for example, mapping "6" to "m", "n", and "o".
    
    Use a backtracking function to generate all possible combinations.
    
    The function should take 2 primary inputs: the current combination of letters we have, path, and the index we are currently checking.
    As a base case, if our current combination of letters is the same length as the input digits, that means we have a complete combination. Therefore, add it to our answer, and backtrack.
    Otherwise, get all the letters that correspond with the current digit we are looking at, digits[index].
    
    Loop through these letters. For each letter, add the letter to our current path, and call backtrack again, but move on to the next digit by incrementing index by 1.
    Make sure to remove the letter from path once finished with it.

---
+++ Backtracking/LetterCombinationsOfPhoneNumber/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+ Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+Input: digits = ""
+Output: []
+
+Input: digits = "2"
+Output: ["a","b","c"]
+ 
+Constraints:
+
+0 <= digits.length <= 4
+digits[i] is a digit in the range ['2', '9'].
+ 
+ */
+
+
+using System.Text;
+
+namespace LetterCombinationsOfPhoneNumber
+{
+    class Program
+    {
+        List<string> combinations = new List<string>();
+        Dictionary<char, string> letters = new Dictionary<char, string>() {
+            { '2', "abc" }, {'3', "def" }, {'4', "ghi" }, {'5', "jkl" }, {'6', "mno" }, {'7', "prqs" }, {'8', "tuv" }, {'9', "wxyz" }
+        };
+        string phoneDigits;
+
+        static void Main(string[] args)
+        {
+            //TODO: implement test cases
+        }
+        public IList<string> LetterCombinations(string digits)
+        {
+            if (digits.Length == 0) return combinations;
+
+            phoneDigits = digits;
+        }
+
+        public void Backtrack(int idx, StringBuilder path)
+        {
+            if (path.Length == phoneDigits.Length)
+            {
+                combinations.Add(path.ToString());
+                return;
+            }
+
+        }
+    }
+}
